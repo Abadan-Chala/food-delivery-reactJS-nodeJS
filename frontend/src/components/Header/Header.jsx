1130c35b723e5a3262a3976e05a0a562b2c2bd2a
abadanaf chala <abadanafchala@gmail.com>
2025-01-16 08:16:12 -0800

    header pege added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite food here</h2>
+            <p>Order your favourite food from your favourite restaurant</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
