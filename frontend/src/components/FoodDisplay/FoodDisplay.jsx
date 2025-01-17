713fa5d23ee5cfba7ab603dd0335bbb03d43e142
abadanaf chala <abadanafchala@gmail.com>
2025-01-17 04:16:25 -0800

    show more and less

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,21 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext)
+  const [showAll, setShowAll] = useState(false)
+
+  const filteredList = food_list.filter(item => category === "All" || category === item.category)
+  
+  const getRandomItems = (list, num) => {
+    const shuffled = [...list].sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, num)
+  }
+
+  const displayedList = showAll ? filteredList : getRandomItems(filteredList, 10)
+
   return (
     <div className="food-display" id='food-display'>
-        <h2>Top Dishes Near You</h2>
-        <div className="food-display-list">
-           {food_list.map((item, index) => {
-            if (category==="All" || category === item.category){
-              return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
-            }
-           })}
-        </div>
-      
+      <h2>Top Dishes Near You</h2>
+      <div className="food-display-list">
+        {displayedList.map((item, index) => (
+          <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+        ))}
+      </div>
+      {!showAll && filteredList.length > 10 && (
+        <button className='show-more' onClick={() => setShowAll(true)}>Show More</button>
+      )}
+      {showAll && (
+        <button className='show-less' onClick={() => setShowAll(false)}>Show Less</button>
+      )}
     </div>
   )
 }
