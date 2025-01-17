83b11210a573acd1b39d1f008a12fe9fa4c8733d
abadanaf chala <abadanafchala@gmail.com>
2025-01-17 06:37:24 -0800

    footer page

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Footer.css'
+import logo from '../../assets/logo.png'
+import facebook from '../../assets/facebook.png'
+import twitter from '../../assets/twitter.png'
+import linkedin from '../../assets/linkedin.png'
+
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={logo} alt="" />
+                <p>Food Delivery Website</p>
+                <div className="footer-social-icon">
+                    <img src={facebook} alt="" />
+                    <img src={twitter} alt="" />
+                    <img src={linkedin} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>HUFOOD</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+
+            </div>
+            <div className="footer-content-right">
+                <h2>Contact Us</h2>
+                <ul>
+                    <li>+251928103575</li>
+                    <li>haramaya@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2025 © haramaya. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
