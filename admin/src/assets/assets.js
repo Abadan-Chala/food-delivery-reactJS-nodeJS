5bf2d5f54d2c8c977403a2718358c21362b3d2ae
abadanaf chala <abadanafchala@gmail.com>
2025-01-22 00:12:18 -0800

    Navbar and Sidebar pages of admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import add from './add.jpg';
+import order from './order.png';
+import parcel from './parcel.png';
+import profile from './profile.png';
+import upload from './upload.png';
+import logo from './logo.png';
+
+export const assets = {
+    logo,
+    add,
+    order,
+    parcel,
+    profile,
+    upload,
+    };
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img src={assets.add} alt="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order} alt="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order} alt="" />
+                <p>Orders</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
