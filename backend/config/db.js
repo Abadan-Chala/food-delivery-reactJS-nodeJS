6bf3cbea920082338fb2f9b155032e8948e84a7c
abadanaf chala <abadanafchala@gmail.com>
2025-03-18 11:49:39 +0300

    db added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-        const conn = await mongoose.connect('mongodb+srv://haramayafoodorder:11223344@cluster0.kkcwd.mongodb.net/hufood-order').then(()=>console.log("DB Connected"));
+        // const conn = await mongoose.connect('mongodb+srv://haramayafoodorder:11223344@cluster0.kkcwd.mongodb.net/hufood-order').then(()=>console.log("DB Connected"));
+
+        const conn = await mongoose.connect('mongodb+srv://ketifood:11223344@cluster0.zq9s1.mongodb.net/hufood-order').then(()=>console.log("Database Connected"));
     
 };
